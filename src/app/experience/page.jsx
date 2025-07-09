cf69f43a2cc0dea0198198c0a4d2cd2123b1430a
Noorulain09 <noorulainkashmiri00@gmail.com>
2025-07-09 23:46:03 +0530

    Initial commit

--- src/app/experience/page.jsx
+++ src/app/experience/page.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { FaCheckCircle } from 'react-icons/fa'
+import { FaCheck } from 'react-icons/fa6'
 
 function Experience() {
   return (
     <div className="bg-white w-full min-h-screen">
       <div className="bg-gradient-to-br from-white via-purple-200 to-purple-900 w-full min-h-screen flex items-center justify-center px-4">
-        
+
         <div className="w-[1000px] bg-white rounded-xl shadow-lg px-6 sm:px-10 py-12 text-gray-800">
           {/* Heading */}
           <div>
@@ -16,7 +16,7 @@ function Experience() {
 
           {/* Body Section */}
           <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 mt-10">
-            
+
             {/* LEFT LIST - Headings */}
             <div>
               <ul className="font-bold text-gray-600 space-y-2">
@@ -32,23 +32,23 @@ function Experience() {
             <div>
               <ul className="font-semibold text-gray-600 space-y-4">
                 <li className="flex items-start gap-2">
-                  <FaCheckCircle className="text-green-600 mt-1" />
+                  <FaCheck className="text-green-600 mt-1" />
                   Created <span className="text-purple-700">&nbsp;mobile-friendly UIs</span> using React & Tailwind.
                 </li>
                 <li className="flex items-start gap-2">
-                  <FaCheckCircle className="text-green-600 mt-1" />
+                  <FaCheck className="text-green-600 mt-1" />
                   Connected frontend with <span className="text-purple-700">&nbsp;Express APIs</span>.
                 </li>
                 <li className="flex items-start gap-2">
-                  <FaCheckCircle className="text-green-600 mt-1" />
+                  <FaCheck className="text-green-600 mt-1" />
                   Set up <span className="text-purple-700">&nbsp;login/signup</span> using Firebase Auth.
                 </li>
                 <li className="flex items-start gap-2">
-                  <FaCheckCircle className="text-green-600 mt-1" />
+                  <FaCheck className="text-green-600 mt-1" />
                   <span className="text-purple-700">Managed data</span> using MongoDB & Mongoose.
                 </li>
                 <li className="flex items-start gap-2">
-                  <FaCheckCircle className="text-green-600 mt-1" />
+                  <FaCheck className="text-green-600 mt-1" />
                   Hosted apps on <span className="text-purple-700">&nbsp;Vercel & Google Cloud</span>.
                 </li>
               </ul>
